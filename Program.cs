3fbeb8372dbef67cbbc1b5d2c17a2485f522a532
云 江 <1019878449@qq.com>
2020-09-08 17:49:26 +0800

    第二次提交

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace HelloWord
 {
@@ -10,7 +11,34 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("这是我的第一个C#程序");
+           // Console.WriteLine("这是我的第一个C#程序");
+            //Console.ReadLine();
+
+           // ConsoleColor console1 = ConsoleColor.DarkBlue;
+            //string s = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Title = "king";
+            Console.WriteLine("这是我的第一个C#程序"/*+Console.Title*/);
+
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+            Console.WriteLine("这是我的第一个C#程序"/*+Console.Title*/);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+            Console.WriteLine("这是我的第一个C#程序"/*+Console.Title*/);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("这是我的第一个C#程序"/*+Console.Title*/);
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("这是我的第一个C#程序"/*+Console.Title*/);
+
+            //Process.Start("C:\\Windows\\System32\\cmd.exe");
             Console.ReadLine();
         }
     }
